878cfb4707fc8831d8c0b70c704bc16678a3844f
Chris Cooke <kooshini@gmail.com>
2015-07-26 23:20:11 +0100

    Initial
    Initial commit

---
+++ Berksfile
@@ -0,0 +1,2 @@
+source "https://api.berkshelf.com" 
+cookbook "newrelic", git: 'git://github.com/escapestudios-cookbooks/newrelic.git', tag: '2.7.2'
---
+++ laravel/recipes/permissions.rb
@@ -0,0 +1,9 @@
+## Recipe to set recursive permissions for the Laravel storage folder /app/storage/*
+node[:deploy].each do |app_name, deploy|
+	if platform_family?('debian')
+		execute "set permissions for #{deploy[:deploy_to]}/current/storage/" do
+		command "sudo chown deploy:www-data -R #{deploy[:deploy_to]}/current/storage/"
+		command "sudo chmod 0777 -Rf #{deploy[:deploy_to]}/current/storage/"
+	  end
+	end
+end
