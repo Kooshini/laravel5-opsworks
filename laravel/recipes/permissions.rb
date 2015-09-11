54f4c2831c2eb7b99c23720a41e7918da07e0af1
Kooshini <kooshini@gmail.com>
2015-09-11 14:51:41 +0100

    changed owners

--- laravel/recipes/permissions.rb
+++ laravel/recipes/permissions.rb
@@ -1,11 +1,11 @@
-## Recipe to set recursive permissions for the Laravel storage folder /app/storage/*
+## Recipe to set recursive permissions for the Laravel storage folder /storage/ and bootstrap folders*
 node[:deploy].each do |app_name, deploy|
 	if platform_family?('debian')
 		execute "set permissions for #{deploy[:deploy_to]}/current/storage/" do
 		command "sudo mkdir -p #{deploy[:deploy_to]}/current/storage/logs"
 		command "sudo chmod 0777 -Rf #{deploy[:deploy_to]}/current/storage/"
-		command "sudo chown deploy:www-data -R #{deploy[:deploy_to]}/current/storage/"
-		command "sudo chown deploy:www-data -R #{deploy[:deploy_to]}/current/bootstrap/cache/"
+		command "sudo chown www-data:www-data -R #{deploy[:deploy_to]}/current/storage/"
+		command "sudo chown www-data:www-data -R #{deploy[:deploy_to]}/current/bootstrap/cache/"
 		command "sudo chmod 0777 -Rf #{deploy[:deploy_to]}/current/bootstrap/cache/"
 	  end
 	end
