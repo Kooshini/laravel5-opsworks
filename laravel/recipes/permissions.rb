7e078b828263cd817d7bb56d7e57bef743aef7a5
Kooshini <kooshini@gmail.com>
2015-09-11 14:36:48 +0100

    create the storage folder

--- laravel/recipes/permissions.rb
+++ laravel/recipes/permissions.rb
@@ -2,9 +2,10 @@
 node[:deploy].each do |app_name, deploy|
 	if platform_family?('debian')
 		execute "set permissions for #{deploy[:deploy_to]}/current/storage/" do
+		command "sudo mkdir -p #{deploy[:deploy_to]}/current/storage/logs"
+		command "sudo chmod 0777 -Rf #{deploy[:deploy_to]}/current/storage/"
 		command "sudo chown deploy:www-data -R #{deploy[:deploy_to]}/current/storage/"
 		command "sudo chown deploy:www-data -R #{deploy[:deploy_to]}/current/bootstrap/cache/"
-		command "sudo chmod 0777 -Rf #{deploy[:deploy_to]}/current/storage/"
 		command "sudo chmod 0777 -Rf #{deploy[:deploy_to]}/current/bootstrap/cache/"
 	  end
 	end
