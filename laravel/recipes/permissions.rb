a944dcb2b9c94c90eb2f1e92c32c9d9c8684802c
Kooshini <kooshini@gmail.com>
2015-09-11 12:22:31 +0100

    Updated for the new laravel 5x cache table
    Used to be .json file in app/storage/meta

--- laravel/recipes/permissions.rb
+++ laravel/recipes/permissions.rb
@@ -3,7 +3,9 @@ node[:deploy].each do |app_name, deploy|
 	if platform_family?('debian')
 		execute "set permissions for #{deploy[:deploy_to]}/current/storage/" do
 		command "sudo chown deploy:www-data -R #{deploy[:deploy_to]}/current/storage/"
+		command "sudo chown deploy:www-data -R #{deploy[:deploy_to]}/current/bootstrap/cache/"
 		command "sudo chmod 0777 -Rf #{deploy[:deploy_to]}/current/storage/"
+		command "sudo chmod 0777 -Rf #{deploy[:deploy_to]}/current/bootstrap/cache/"
 	  end
 	end
 end
