dccf060f76d2c05552ca96b9141dd43a8171ccbc
Kooshini <kooshini@gmail.com>
2015-09-11 21:50:33 +0100

    Update permissions.rb

--- laravel/recipes/permissions.rb
+++ laravel/recipes/permissions.rb
@@ -1,14 +1,14 @@
 ## Recipe to set recursive permissions for the Laravel storage folder /storage and /bootstrap folders*
 	node[:deploy].each do |app_name, deploy|
 		execute "set permissions for storage and cache folders" do
-			directory "#{deploy[:deploy_to]}/storage" do 
+			directory "#{deploy[:deploy_to]}/current/storage" do 
 			  mode 0777
 			  owner 'www-data'
 			  group 'www-data'
 			  recursive true
 			  action :create
 			end
-			directory "#{deploy[:deploy_to]}/bootstrap/cache" do 
+			directory "#{deploy[:deploy_to]}/current/bootstrap/cache" do 
 			  mode 0777
 			  owner 'www-data'
 			  group 'www-data'
